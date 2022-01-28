71b09f1d5379fd1f53e366c085454da545e8e9a1
Wanderson Santos <wanderlavo@gmail.com>
2022-01-27 21:17:35 -0300

    SHADOW_DATABASE_URL

--- prisma/migrations/20220127231800_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE `Endereco` (
-    `id` INTEGER NOT NULL,
-    `nome` VARCHAR(191) NOT NULL,
-    `rua` VARCHAR(191) NOT NULL,
-
-    UNIQUE INDEX `Endereco_id_key`(`id`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- prisma/migrations/20220127232334_add_autoincrement_id/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX `Endereco_id_key` ON `endereco`;
-
--- AlterTable
-ALTER TABLE `endereco` MODIFY `id` INTEGER NOT NULL AUTO_INCREMENT;
